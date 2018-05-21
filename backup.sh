bd7557f776656a58c473af45966f52b7230ec791
Vishnu Nair <vnair@Vishnus-MacBook-Pro.local>
2018-05-21 16:19:51 +0400

    ship it

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+EMAIL=""
+FOLDER=""
+printhelp() {
+
+echo "
+Usage: sh backup.sh [OPTION]...
+  --email    Provide your email for backup
+  --folder   Provide location of your folder
+  -h, --help Display help file
+"
+
+}
+while [ "$1" != "" ]; do
+  case "$1" in
+    --email )        EMAIL=$2; shift 2 ;;
+    --folder )       FOLDER=$2; shift 2 ;;
+    -h    | --help )	        echo "$(printhelp)"; exit; shift; break ;;
+  esac
+done
+
+cd ~ && git clone git@github.com:vishnudxb/movemail.git && \
+        cd movemail && ./gyb --email $EMAIL --local-folder "$FOLDER"
---
+++ restore.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+EMAIL=""
+FOLDER=""
+printhelp() {
+
+echo "
+Usage: sh backup.sh [OPTION]...
+  --email    Provide your email for backup
+  --folder   Provide location of your folder
+  -h, --help Display help file
+"
+
+}
+while [ "$1" != "" ]; do
+  case "$1" in
+    --email )        EMAIL=$2; shift 2 ;;
+    --folder )       FOLDER=$2; shift 2 ;;
+    -h    | --help )	        echo "$(printhelp)"; exit; shift; break ;;
+  esac
+done
+
+cd ~/movemail && ./gyb --email $EMAIL --action restore --local-folder "$FOLDER"
